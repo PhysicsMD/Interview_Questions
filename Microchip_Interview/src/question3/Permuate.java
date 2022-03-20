b2b3501145407a7a817acfd4ccda3f2384db707d
Alenn Wright <bowserko123@gmail.com>
2022-03-20 10:24:31 -0700

    Update Permuate.java
    added proper comments

--- Microchip_Interview/src/question3/Permuate.java
+++ Microchip_Interview/src/question3/Permuate.java
@@ -4,41 +4,67 @@ import java.util.ArrayList;
 
 public class Permuate {
 	
+	/**
+	    * ArrayList<String> permutationList;
+	    * list for all the permutation combinations
+	    */
 	ArrayList<String> permutationList;
 	
+	/**
+	    * public Permuate(int[] intList)
+	    * initializes the list of permutations
+	    * then calls recursive helper
+	    */
 	public Permuate(int[] intList) 
 	{
 		permutationList = new ArrayList<String>();
 		PermuateHelper(intList, "");
 	}
 	
+	/**
+	    * private void PermuateHelper(int[] intList, String runningString)
+	    * for each item in the list, the recursive function breaks to another
+	    * iteration until there is only one item left in the list
+	    */
 	private void PermuateHelper(int[] intList, String runningString) 
 	{
+		//check for only one item left in the intList
 		if(intList.length == 1) 
 		{
 			permutationList.add(runningString);
 		}
 		else
 		{
-			for(int i = 0; i < intList.length ; i++) 
+			//iterates through the whole list of integers
+			for(int branchiterator = 0; branchiterator < intList.length ; branchiterator++) 
 			{
+				//create a temporary list to pass into the recursive helper
 				int[] helperList = new int[intList.length -1];
-				for(int j = 0; j < intList.length-1; j++) 
+				
+				//for loop to remove the branch integer from next integer list
+				for(int integerlistIterator = 0; integerlistIterator < intList.length-1; integerlistIterator++) 
 				{
-					if(j<i) 
+					//fill list for all the items before the current integer
+					if(integerlistIterator<branchiterator) 
 					{
-						helperList[j] = intList[j];
+						helperList[integerlistIterator] = intList[integerlistIterator];
 					}
 					else 
 					{
-						helperList[j] = intList[j+1];
+						//conditional for filling the list with integers after the branch iterator
+						helperList[integerlistIterator] = intList[integerlistIterator+1];
 					}
 				}
-				PermuateHelper(helperList, runningString + intList[i]);
+				//call the helper to the branch
+				PermuateHelper(helperList, runningString + intList[branchiterator]);
 			}
 		}
 	}
 	
+	/**
+	    * public void printList() 
+	    * prints the permutations into the console
+	    */
 	public void printList() 
 	{
 		System.out.println("Permutations List:");
