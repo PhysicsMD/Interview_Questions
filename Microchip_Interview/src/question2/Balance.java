677726ae5fb8c54ff7bea8afb4c6cc47a6a3fbfd
Alenn Wright <bowserko123@gmail.com>
2022-03-20 09:52:45 -0700

    Update Balance.java
    added comments to add readability of the method

--- Microchip_Interview/src/question2/Balance.java
+++ Microchip_Interview/src/question2/Balance.java
@@ -4,19 +4,18 @@ package question2;
  * 
  * @author Alenn Wright
  *
- *public class Balance extends Object
+ *public class Balance
  *
- *Description: Class wrapper for a Java stack class 
- *following filo scheme for parsing a string of brackets
- *ensuring they are balanced
+ *Description: Class wrapper for a Java stack class for checking 
+ *for balanced strings of brackets
  *
  */
 
 public class Balance {
 	
-	String bracketTypes = "({[)}]"; 
+	private String bracketTypes = "({[)}]"; 
 	
-	public Balance() {}
+	private Balance() {}
 	
 	public boolean isBalance(String UserInput)
 	{
